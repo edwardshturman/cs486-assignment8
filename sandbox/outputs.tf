c6c2eb0ac73f6e486edc61a66ec1086ee95425e5
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-03-30 16:45:12 -0700

    Use Modules to populate a VPC and EC2 instances

--- sandbox/outputs.tf
+++ sandbox/outputs.tf
@@ -1,9 +1,19 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.example.id
+output "instance_private_ips" {
+  description = "Private IP addresses of the EC2 instances"
+  value       = module.ec2_instances[*].private_ip
 }
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.example.public_ip
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_public_subnets" {
+  description = "List of IDs of the VPC's public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "vpc_private_subnets" {
+  description = "List of IDs of the VPC's private subnets"
+  value       = module.vpc.private_subnets
 }
--- sandbox/variables.tf
+++ sandbox/variables.tf
@@ -1,5 +1,29 @@
-variable "instance_name" {
-  description = "Value of the name tag for the EC2 instance"
+variable "instance_prefix" {
+  description = "A prefix for the Name tag for the EC2 instance cluster"
   type        = string
-  default     = "ExampleInstance"
+  default     = "cs486-assignment8-edwardshturman"
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+  default     = 6
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC to use for the EC2 instance cluster"
+  type        = string
+  default     = "cs486-assignment8-edwardshturman-vpc"
+}
+
+variable "allow_ssh_ip" {
+  description = "An IPv4 from which SSH access is allowed"
+  type        = string
+  default     = ""
+}
+
+variable "public_key" {
+  description = "Public key for SSH access to the bastion host"
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDzPKTwRtGWT74BNpNSHslV5t7X4Ehma83w3Fc6d/4APvaHVOdMrvfFPO/bUxbzLLbyd3kpk4lnNnsQG+VZhIh9IUC0i13c1RHh+dXIhXwlxdfEHfPfCdqwCp0G1fmu3D5jac/5yUc3YwQletlwwEKZxiBdiHAd1yElXzDXSZMxV75Y4FBILZkvyjiPAGGcWJf2mpfIXjq/1gtCvXAUlLL0ZXsHfOd3SmApVDh73A1/SaL62W1ZOIHRH3PxqLyrCTq2awS9IbszHvewuLDyMa9CdJG7tOEbjwT855obZFx+1dNOiLmxQUBTAY+3GJ2U/AwIWiPnjwgLQihLsrXW282t"
 }
