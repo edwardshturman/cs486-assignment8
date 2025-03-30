a580b79d2d0d8de07521101e2aea7dcb5a75f14d
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-03-30 16:40:01 -0700

    Move Terraform settings to separate file

--- sandbox/main.tf
+++ sandbox/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.2.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-west-1"
 }
